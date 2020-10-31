b0fb75a848e2a296c56345a8c9bbbaf54d6e04a1
IMBlackPanther <mailsagarat999@gmail.com>
2020-10-31 11:27:02 +0530

    signup page and user health 2

--- Android/app/src/main/java/com/bugisbae/bloodhive/MainActivity.java
+++ Android/app/src/main/java/com/bugisbae/bloodhive/MainActivity.java
@@ -4,11 +4,15 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 import androidx.drawerlayout.widget.DrawerLayout;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
 
 public class MainActivity extends AppCompatActivity {
     Toolbar toolbar;
     DrawerLayout drawer;
+    ImageButton myFeed,myAccount;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,5 +27,19 @@ public class MainActivity extends AppCompatActivity {
             getSupportActionBar().setTitle("BloodHive");
 
         }
+
+        myAccount=(ImageButton)findViewById(R.id.myAccountButton);
+        myFeed=(ImageButton)findViewById(R.id.myFeed);
+
+        myAccount.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(getApplicationContext(),UserHealth.class));
+            }
+        });
+
+
     }
+
+
 }
